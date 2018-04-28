a7d67384a5b92e1ee2c366e9cefe2881d8fc335f
Allen Yang <shentao.yang@outlook.com>
2018-04-28 13:31:28 +0800

    Build a common Redux structure for React Native app

--- PlanA/index.js
+++ PlanA/index.js
@@ -1,4 +1,5 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import { AppContainer } from "./src/app";
 
-AppRegistry.registerComponent('PlanA', () => App);
+AppRegistry.registerComponent('PlanA', () => AppContainer);
